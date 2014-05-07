8309933241bbf7f8a3d47fbae8290dcf8e296171
Anna Weber <anna.m.weber@gmail.com>
2014-05-07 10:21:51 +0200

    [PB, RL, AW] {other} Initial commit.

---
+++ projects/TrackMe_Test/src/at/software2014/trackme/test/StartActivityTest.java
@@ -0,0 +1,30 @@
+package at.software2014.trackme.test;
+
+import at.software2014.trackme.*;
+
+import com.robotium.solo.Solo;
+import android.test.ActivityInstrumentationTestCase2;
+
+public class StartActivityTest extends ActivityInstrumentationTestCase2<StartActivity> {
+
+	private Solo mSolo;
+
+	public StartActivityTest() {
+		super(StartActivity.class);
+	}
+
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		mSolo = new Solo(getInstrumentation(), getActivity());
+	}
+
+	@Override
+	protected void tearDown() throws Exception {
+		// TODO Auto-generated method stub
+		super.tearDown();
+	}
+
+	
+	
+}
