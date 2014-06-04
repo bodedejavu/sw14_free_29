d4b4f6c7778ec46d6a886035d4840284bff5b414
Anna Weber <anna.m.weber@gmail.com>
2014-06-04 13:31:18 +0200

    [AW] {other} Initial commit of App Engine skeleton code.

---
+++ projects/gcmtest-AppEngine/src/at/joanneum/gcmtest/Lobster.java
@@ -0,0 +1,10 @@
+package at.joanneum.gcmtest;
+public class Lobster {
+  private int whiskerLength;
+  public int getWhiskerLength() {
+    return whiskerLength;
+  }
+  public void setWhiskerLength(int whiskerLength) {
+    this.whiskerLength = whiskerLength;
+  }
+}
