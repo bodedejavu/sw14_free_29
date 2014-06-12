cfedd0f0421b99429d63c60c77777f473f196c1a
rainer-lankmayr <microft1@gmx.at>
2014-06-12 17:59:58 +0200

    [RL] {other} finished implementaion of addContactActivity, added confirmationDialog, handling in contactsFragment

--- projects/TrackMe/src/at/software2014/trackme/AddContactActivity.java
+++ projects/TrackMe/src/at/software2014/trackme/AddContactActivity.java
@@ -3,6 +3,9 @@ package at.software2014.trackme;
 import java.util.ArrayList;
 import java.util.List;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -10,7 +13,7 @@ import android.widget.ListView;
 import android.widget.AdapterView.OnItemClickListener;
 
 public class AddContactActivity extends BaseActivity {
-	
+
 	private ListView mListView;
 	private ContactsAdapter mListAdapter;
 	private ContactsItem mSelectedItem;
@@ -23,34 +26,77 @@ public class AddContactActivity extends BaseActivity {
 		mListAdapter = new ContactsAdapter(this);
 		mListView = (ListView) findViewById(R.id.contacts_add_listView);
 		mListView.setAdapter(mListAdapter);
-		mListView.setSelector(R.drawable.selector_background);
-		
+
 		setListData();
-		
+
 		mListView.setOnItemClickListener(new OnItemClickListener() {
 			@Override
 			public void onItemClick(AdapterView<?> parent, View mListView,
 					int position, long id) {
 				mSelectedItem = mListAdapter.getItem(position);
+				showAffirmationDialog();
 			}
 		});
 	}
-	
+
+	private void showAffirmationDialog() {
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+		builder.setTitle(R.string.action_contact_add_dialog_title);
+		builder.setMessage(R.string.action_contact_add_dialog_text);
+		builder.setCancelable(false);
+		builder.setPositiveButton(R.string.confirm,
+				new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int id) {
+						finish();
+					}
+				});
+		builder.setNegativeButton(R.string.cancel,
+				new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int id) {
+						mSelectedItem = null;
+						mListView.clearChoices();
+						mListView.requestLayout();
+						dialog.cancel();
+					}
+				});
+
+		AlertDialog alertDialog = builder.create();
+		alertDialog.show();
+	}
+
 	private void setListData() {
 		List<ContactEntry> contacts = new ArrayList<ContactEntry>();
 		ArrayList<ContactsItem> contactsList = new ArrayList<ContactsItem>();
-		
-		contacts.add(new ContactEntry("Anna Weber", "anna.weber@gmail.com", (long)1401216003*1000, 47.1, 15.4));
-		contacts.add(new ContactEntry("Rainer Lankmayr", "rainer.lankmayr@gmail.com", (long)1401215993*1000, 47.0, 15.5));
-		contacts.add(new ContactEntry("Benjamin Steinacher", "benjamin.steinacher@gmail.com", (long)1401216000*1000, 47.08, 15.35));
-		
-    	for (int i=0; i < contacts.size(); i++) {
-    		ContactEntry contactEntry = contacts.get(i);    		
-    		String eMail = contactEntry.geteMail();
-    		String name = contactEntry.getName();
-    		contactsList.add(new ContactsItem(eMail, name));
-    	}
-    	
+
+		contacts.add(new ContactEntry("Anna Weber", "anna.weber@gmail.com",
+				(long) 1401216003 * 1000, 47.1, 15.4));
+		contacts.add(new ContactEntry("Rainer Lankmayr",
+				"rainer.lankmayr@gmail.com", (long) 1401215993 * 1000, 47.0,
+				15.5));
+		contacts.add(new ContactEntry("Benjamin Steinacher",
+				"benjamin.steinacher@gmail.com", (long) 1401216000 * 1000,
+				47.08, 15.35));
+
+		for (int i = 0; i < contacts.size(); i++) {
+			ContactEntry contactEntry = contacts.get(i);
+			String eMail = contactEntry.geteMail();
+			String name = contactEntry.getName();
+			contactsList.add(new ContactsItem(eMail, name));
+		}
+
 		mListAdapter.setData(contactsList);
+	}
+
+	@Override
+	public void finish() {
+		if (mSelectedItem != null) {
+			Intent data = new Intent();
+			data.putExtra("eMail", mSelectedItem.getKey());
+			setResult(RESULT_OK, data);
+		} else {
+			setResult(RESULT_CANCELED);
+		}
+		super.finish();
 	}
 }
--- projects/TrackMe/src/at/software2014/trackme/ContactsFragment.java
+++ projects/TrackMe/src/at/software2014/trackme/ContactsFragment.java
@@ -26,7 +26,8 @@ import android.widget.AdapterView.OnItemClickListener;
 public class ContactsFragment extends Fragment {
 
 	private static final String ARG_SECTION_NUMBER = "section_number";
-	private static final int CONTACT_PICKER_RESULT = 1001;
+	private static final int REQUEST_CODE_CONTACT_PICKER = 1001;
+	private static final int REQUEST_CODE_ADD = 1002;
 	private ListView mListView;
 	private ContactsAdapter mListAdapter;
 	private ContactsItem mSelectedItem;
@@ -66,7 +67,7 @@ public class ContactsFragment extends Fragment {
 	@Override
 	public void onViewCreated(View v, Bundle savedInstanceState) {
 		super.onViewCreated(v, savedInstanceState);
-		
+
 		setListData();
 	}
 
@@ -105,30 +106,31 @@ public class ContactsFragment extends Fragment {
 	}
 
 	private void setListData() {
-		List<ContactEntry> contacts = ((MainActivity)getActivity()).getContacts();
+		List<ContactEntry> contacts = ((MainActivity) getActivity())
+				.getContacts();
 		ArrayList<ContactsItem> contactsList = new ArrayList<ContactsItem>();
-		
-    	for (int i=0; i < contacts.size(); i++) {
-    		ContactEntry contactEntry = contacts.get(i);    		
-    		String eMail = contactEntry.geteMail();
-    		String name = contactEntry.getName();
-    		contactsList.add(new ContactsItem(eMail, name));
-    	}
-    	
+
+		for (int i = 0; i < contacts.size(); i++) {
+			ContactEntry contactEntry = contacts.get(i);
+			String eMail = contactEntry.geteMail();
+			String name = contactEntry.getName();
+			contactsList.add(new ContactsItem(eMail, name));
+		}
+
 		mListAdapter.setData(contactsList);
 	}
 
 	private void inviteContact() {
 		Intent contactPickerIntent = new Intent(Intent.ACTION_PICK,
 				ContactsContract.Contacts.CONTENT_URI);
-		startActivityForResult(contactPickerIntent, CONTACT_PICKER_RESULT);
+		startActivityForResult(contactPickerIntent, REQUEST_CODE_CONTACT_PICKER);
 	}
 
 	@Override
 	public void onActivityResult(int requestCode, int resultCode, Intent data) {
-		if (resultCode == Activity.RESULT_OK) {
-			switch (requestCode) {
-			case CONTACT_PICKER_RESULT:
+		switch (requestCode) {
+		case REQUEST_CODE_CONTACT_PICKER:
+			if (resultCode == Activity.RESULT_OK) {
 				String email = "";
 				Uri result = data.getData();
 				String id = result.getLastPathSegment();
@@ -136,26 +138,43 @@ public class ContactsFragment extends Fragment {
 				Cursor cursor = getActivity().getContentResolver().query(
 						Email.CONTENT_URI, null, Email.CONTACT_ID + "=?",
 						new String[] { id }, null);
-				// cursor.moveToFirst();
 
 				if (cursor.moveToFirst()) {
 					int emailIdx = cursor.getColumnIndex(Email.DATA);
 					email = cursor.getString(emailIdx);
 				}
 
-				if (email.length() == 0) {
+				if (email.length() != 0) {
+					openInvitationMail(email);
+				} else {
 					Toast.makeText(getActivity(),
 							R.string.action_contact_invite_no_email,
 							Toast.LENGTH_LONG).show();
-				} else {
-					openInvitationMail(email);
 				}
-				break;
+			} else if (resultCode == Activity.RESULT_CANCELED) {
+				// do nothing
+			} else {
+				Toast.makeText(getActivity(),
+						R.string.action_contact_invite_cp_fail,
+						Toast.LENGTH_LONG).show();
 			}
-		} else {
-			Toast.makeText(getActivity(),
-					R.string.action_contact_invite_cp_fail, Toast.LENGTH_LONG)
-					.show();
+			break;
+
+		case REQUEST_CODE_ADD:
+			if (resultCode == Activity.RESULT_OK) {
+				// TODO send eMail
+				//String eMail = data.getExtras().getString("eMail");
+				Toast.makeText(getActivity(), getResources()
+						.getString(R.string.action_contact_add_in_progress), 
+						Toast.LENGTH_LONG).show();
+			} else if (resultCode == Activity.RESULT_CANCELED) {
+				// do nothing
+			} else {
+				Toast.makeText(getActivity(),
+						R.string.action_contact_add_failed, Toast.LENGTH_LONG)
+						.show();
+			}
+			break;
 		}
 	}
 
@@ -164,20 +183,17 @@ public class ContactsFragment extends Fragment {
 		Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);
 		emailIntent.setType("plain/text");
 		emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, aEmailList);
-		emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT,
-				"TrackMe Invitation");
-		emailIntent.putExtra(android.content.Intent.EXTRA_TEXT,
-				"Hello,\n i am inviting you to download and test the APP TrackMe.\n link");
+		emailIntent
+				.putExtra(android.content.Intent.EXTRA_SUBJECT, getResources()
+						.getString(R.string.action_contact_invite_suject));
+		emailIntent.putExtra(android.content.Intent.EXTRA_TEXT, getResources()
+				.getString(R.string.action_contact_invite_text));
 		startActivity(emailIntent);
 	}
 
 	private void addContact() {
-		//TODO implement add
 		Intent intent = new Intent(getActivity(), AddContactActivity.class);
-	    startActivity(intent);
-	    
-		Toast.makeText(getActivity(), R.string.action_contact_add_successful,
-				Toast.LENGTH_LONG).show();
+		startActivityForResult(intent, REQUEST_CODE_ADD);
 	}
 
 	private void deleteContact() {
